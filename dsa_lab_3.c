4b3b2927770ae51f5685234bc33deed5aa662dd3
ellgenta <lemurkanikitka@mail.ru>
2025-03-08 21:25:04 +0200

    all functions were written, an error handling system will be implemented soon

--- dsa_lab_3.c
+++ dsa_lab_3.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <assert.h>
 
+typedef enum{
+    false,
+    true
+} bool;
+
 typedef struct _node{
     int data;
     struct _node* prev;
@@ -46,6 +51,42 @@ void pop(linked_list* queue){
 
 }
 
+size_t size(linked_list* queue){
+    if(!queue->front && !queue->rear)
+        return 0;
+    
+    node* aux_ptr=queue->front;
+
+    size_t count=0;
+    while(aux_ptr){
+        aux_ptr=aux_ptr->next;
+        ++count;
+    }
+
+    return count;
+}
+
+bool empty(linked_list* queue){
+    if(!queue->front && !queue->rear)
+        return true;
+    else
+        return false;
+}
+
+int front(linked_list* queue){
+    if(empty(queue))
+        return -1; //error handling will be added later
+    else
+        return queue->front->data;
+}
+
+int back(linked_list* queue){
+    if(empty(queue))
+        return -1; //eror handling will be added later
+    else
+        return queue->rear->data;
+}   
+
 void test_push(){
     linked_list q={0};
 
@@ -88,10 +129,98 @@ void test_pop(){
     assert(!q.rear);
 }
 
+void test_size(){
+    linked_list q={calloc(1,sizeof(node)),calloc(1,sizeof(node))};
+    assert(q.front);
+    assert(q.rear);
+    node* sec_node=malloc(sizeof(node));
+    assert(sec_node);
+
+    q.front->next=sec_node;
+    sec_node->prev=q.front;
+    sec_node->next=q.rear;
+    q.rear->prev=sec_node;
+
+    assert(size(&q)==3);
+    
+    free(q.rear);
+    q.rear=sec_node;
+    q.rear->next=NULL;
+    assert(size(&q)==2);
+
+    free(q.rear);
+    q.rear=q.front;
+    q.rear->next=NULL;
+    assert(size(&q)==1);
+
+    free(q.rear);
+    q.rear=NULL;
+    q.front=NULL;
+    assert(size(&q)==0);
+}
+
+void test_empty(){
+    linked_list q={calloc(1,sizeof(node))};
+    assert(q.front);
+    q.rear=q.front;
+
+    assert(empty(&q)==false);
+
+    free(q.front);
+    q.front=NULL;
+    q.rear=NULL;
+    assert(empty(&q)==true);
+}
+
+void test_front(){
+    linked_list q={calloc(1,sizeof(node)),calloc(1,sizeof(node))};
+    assert(q.front);
+    q.front->next=q.rear;
+    q.rear->prev=q.front;
+    q.front->data=1;
+
+    assert(front(&q)==1);
+
+    free(q.front);
+    q.front=q.rear;
+    q.rear->prev=NULL;
+    assert(front(&q)==0);
+
+    free(q.front);
+    q.front=NULL;
+    q.rear=NULL;
+    //a test with !q.front && !q.rear coming up
+}
+
+void test_back(){
+    linked_list q={calloc(1,sizeof(node)),calloc(1,sizeof(node))};
+    assert(q.front);
+    q.front->next=q.rear;
+    q.rear->prev=q.front;
+    q.front->data=1;
+    q.rear->data=2;
+
+    assert(back(&q)==2);
+
+    free(q.rear);
+    q.rear=q.front;
+    q.rear->next=NULL;
+    assert(back(&q)==1);
+
+    free(q.front);
+    q.front=NULL;
+    q.rear=NULL;
+    //a test with !q.front && !q.rear coming up
+}
+
 int main(){
 
     test_push();
     test_pop();
-    
+    test_size();
+    test_empty();
+    test_front();
+    test_back();
+
     return 0;
 }

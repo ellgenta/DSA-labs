a097cf2615a1886bece4e8435b6c7db853e91fa3
ellgenta <lemurkanikitka@mail.ru>
2025-04-21 22:36:11 +0300

    max_heapify() & cons_heap() functions added
    *First of all, it is important to mention, that the purpose of this laboratory work is to create an "interface " (a set of member functions) for the heapsort implementation
    
    1. left() and right() functions were implemented to find the indices of corresponding descendants of the i-th node
    2. max_heapify() function, that is used for carrying out rotations in heap, was added
    3. appended cons_heap(), that turns the initial array into a heap

--- dsa_lab_6.c
+++ dsa_lab_6.c
@@ -1,1 +1,50 @@
+#include <stdlib.h> 
+#include <stdio.h> 
 
+void swap(int* x,int* y){
+    int t=*x;
+
+    *x=*y;
+    *y=t;
+}
+
+int left(int i){
+    return i*2+1;
+}
+
+int right(int i){
+    return i*2+2;
+}
+
+void max_heapify(int* ar,size_t len,int i){
+    if(i>=len)
+        return;
+
+    int l=left(i);
+    int r=right(i);
+    int t=i;
+
+    if(l<len && ar[l]>ar[t])
+        t=l;
+    if(r<len && ar[r]>ar[t])
+        t=r;
+
+    if(t!=i){
+        swap(&ar[i],&ar[t]);
+        max_heapify(ar,len,t);
+    }   
+}
+
+void cons_heap(int* ar,int len){
+    if(len<0)
+        return;
+    
+    for(int i=len/2-1;i>=0;--i)
+        max_heapify(ar,len,i);
+}
+
+int main(void)
+{
+    
+    return 0;
+}

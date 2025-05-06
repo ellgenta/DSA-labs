1d52d65b7b2b98bcb223ac46e0ad4546710a0f05
ellgenta <lemurkanikitka@mail.ru>
2025-05-07 00:22:44 +0300

    explicit indication of sorting order added
    1. additional parameter for explicit indication of expected sorting order
    1.1 ascending(), descending() were added
    1.2 some new test-cases were implemented

--- dsa_lab_6.c
+++ dsa_lab_6.c
@@ -27,7 +27,15 @@ int right(int i){
     return i*2+2;
 }
 
-void max_heapify(int* ar,ssize_t len,int i){
+bool ascending(int x,int y){
+    return (x-y>0) ? true : false;
+}
+
+bool descending(int x,int y){
+    return (x-y<0) ? true : false;
+}
+
+void max_heapify(int* ar,ssize_t len,int i,bool(*opt)(int,int)){
     if(i>=len || len<=0)
         return;
 
@@ -35,62 +43,78 @@ void max_heapify(int* ar,ssize_t len,int i){
     int r=right(i);
     int t=i;
 
-    if(l<len && ar[l]>ar[t])
+    if(l<len && opt(ar[l],ar[t]))
         t=l;
-    if(r<len && ar[r]>ar[t])
+    if(r<len && opt(ar[r],ar[t]))
         t=r;
 
     if(t!=i){
         swap(&ar[i],&ar[t]);
-        max_heapify(ar,len,t);
+        max_heapify(ar,len,t,opt);
     }   
 }
 
-void heap_sort(int* ar,ssize_t len){
-    if(len<=0)
+void heap_sort(int* ar,ssize_t len,bool(*opt)(int,int)){
+    if(len<=0 || len==1)
         return;
 
     int cur_len=len;
 
     for(int i=len/2-1;i>=0;--i)
-        max_heapify(ar,len,i);
+        max_heapify(ar,len,i,opt);
 
     for(int i=0;i<len;++i){
         swap(&ar[cur_len-1],&ar[0]);
-        max_heapify(ar,--cur_len,0);
+        max_heapify(ar,--cur_len,0,opt);
     }
 }
 
 void test_heap_sort(){
-    int ft[10]={7,4,3,5,8,9,2,6,1,0};
-    int exp_ft[10]={0,1,2,3,4,5,6,7,8,9};
-    heap_sort(ft,10);
-    assert(arcmp(ft,exp_ft,10));
+    int first[10]={7,4,3,5,8,9,2,6,1,0};
+    int exp_first[10]={0,1,2,3,4,5,6,7,8,9};
+    heap_sort(first,10,ascending);
+    assert(arcmp(first,exp_first,10*sizeof(int))==0);
 
-    int sd[1]={1};
-    int exp_sd[1]={1};
-    heap_sort(sd,1);
-    assert(arcmp(sd,exp_sd,1));
+    int second[1]={1};
+    int exp_second[1]={1};
+    heap_sort(second,1,ascending);
+    assert(arcmp(second,exp_second,1));
 
-    int td[10]={0};
-    int exp_td[10]={0};
-    heap_sort(td,10);
-    assert(arcmp(td,exp_td,10));
+    int third[10]={0};
+    int exp_third[10]={0};
+    heap_sort(third,10,ascending);
+    assert(arcmp(third,exp_third,10));
 
-    int fh[10]={-10,85,6,-28,31,0,14,44,9,13};
-    int exp_fh[10]={-28,-10,0,6,9,13,14,31,44,85};
-    heap_sort(fh,10);
-    assert(arcmp(fh,exp_fh,10));
+    int fourth[10]={-10,85,6,-28,31,0,14,44,9,13};
+    int exp_fourth[10]={-28,-10,0,6,9,13,14,31,44,85};
+    heap_sort(fourth,10,ascending);
+    assert(arcmp(fourth,exp_fourth,10));
 
-    int ff[5]={3,2,1,5,6};
-    int exp_ff[5]={3,2,1,5,6};
-    heap_sort(ff,-1);
-    assert(arcmp(ff,exp_ff,5));
+    int fifth[5]={3,2,1,5,6};
+    int exp_fifth[5]={3,2,1,5,6};
+    heap_sort(fifth,-1,ascending);
+    assert(arcmp(fifth,exp_fifth,5));
 
-    int sh[2]={2,1};
-    int exp_sh[2]={1,2};
-    heap_sort(sh,2);
-    assert(arcmp(sh,exp_sh,2));
+    int sixsth[2]={2,1};
+    int exp_sixsth[2]={1,2};
+    heap_sort(sixsth,2,ascending);
+    assert(arcmp(sixsth,exp_sixsth,2));
+
+    int seventh[5]={4,2,0,9,7};
+    int exp_seventh[5]={9,7,4,2,0};
+    heap_sort(seventh,5,descending);
+    assert(arcmp(seventh,exp_seventh,5));
+
+    int eighth[10]={1,-14,28,-35,89,65,0,62,34,34};
+    int exp_eighth[10]={89,65,62,34,34,28,1,0,-14,-35};
+    heap_sort(eighth,10,descending);
+    assert(arcmp(eighth,exp_eighth,10));
+
+    heap_sort(second,1,descending);
+    assert(arcmp(second,exp_second,1));
+
+    heap_sort(third,10,descending);
+    assert(arcmp(third,exp_third,10));
 }
 
 int main(void)
